b7248d349472ff57f8f2f4c79f2c4ffa86344f89
bugsounet <github@bugsounet.fr>
2024-07-02 01:29:56 +0200

    move getJoke to node_helper to prevent error 401/cors error

---
+++ node_helper.js
@@ -0,0 +1,39 @@
+"use strict"
+
+var NodeHelper = require("node_helper")
+
+module.exports = NodeHelper.create({
+  socketNotificationReceived: function (noti, payload) {
+    switch (noti) {
+      case "INIT":
+        console.log("[JOKE] MMM-BlaguesAPI Version:", require('./package.json').version)
+        this.config = payload
+        this.getJoke();
+        setInterval(() => {
+          this.getJoke();
+        }, this.config.fetchInterval);
+        break
+    }
+  },
+  getJoke() {
+      console.info("getJoke");
+      let apiUrl;
+      if (this.config.type === 'random') {
+          apiUrl = 'https://www.blagues-api.fr/api/random';
+      } else {
+          apiUrl = `https://www.blagues-api.fr/api/type/${this.config.type}/random`;
+      }
+      fetch(apiUrl, {
+          method: 'get',
+          headers: new Headers({
+              'Authorization': 'Bearer ' + `${this.config.blaguesApiToken}`,
+              'Content-Type': 'application/x-www-form-urlencoded'
+          })
+      }).then((response) => {
+          response.json().then((joke) => {
+              this.sendSocketNotification("JOKE", joke);
+          });
+      });
+  }
+
+})
